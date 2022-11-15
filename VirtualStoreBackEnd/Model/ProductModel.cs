5777ae358f92f97584c762f571439d4f01c1fbd2
Vinicius Pedro Bisol <vinybisol@gmail.com>
2022-11-15 20:23:04 -0300

    Ajustado codigo para receber as requisições

--- VirtualStoreBackEnd/Model/ProductModel.cs
+++ VirtualStoreBackEnd/Model/ProductModel.cs
@@ -1,47 +1,54 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Buffers;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace VirtualStoreBackEnd.Model
 {
     [Table("product")]
     public class ProductModel
     {
-        [Key]        
+        //[Key]        
         public Guid Id { get; set; } = default!;
-        [Column("full_name")]
-        [Required]
-        [MaxLength(100)]
-        public string FullName { get; set; } = default!;
 
         [Column("name")]
-        [Required]
+        //[Required]
+        [JsonPropertyName("ImageTest")]
         [MaxLength(80)]        
         public string Name { get; set; } = default!;
 
+        [Column("short_name")]
+        //[Required]
+        [MaxLength(100)]
+        public string ShortName { get; set; } = default!;
+
         [Column("price")]
-        [Required]
+        //[Required]
         [Precision(8,2)]
         [ConcurrencyCheck()]
         public decimal Price { get; set; } = default;
 
-        [Column("market_price")]
+        [Column("price_market")]
         [Precision(8, 2)]
         [ConcurrencyCheck()]
-        public decimal MarketPrice { get; set; } = default;
+        public decimal PriceMarket { get; set; } = default;
 
-        [Column("tag")]
-        [Required]
-        [MaxLength(80)]
-        public string TagId { get; set; } = default!;
+        //[Column("tag")]
+        ////[Required]
+        //[MaxLength(80)]
+        //public string TagId { get; set; } = default!;
 
-        [Column("obs")]
+        [Column("note")]
         [MaxLength(255)]
-        public string Obs { get; set; } = default!;
+        public string Note { get; set; } = default!;
 
-        [Column("market_obs")]
-        [MaxLength(255)]
-        public string MarketObs { get; set; } = default!;
+        //[Column("market_obs")]
+        //[MaxLength(255)]
+        //public string MarketObs { get; set; } = default!;
+
+        [Column("image_test")]
+        public Object ImageTest { get; set; } = default!;
 
     }
 }
